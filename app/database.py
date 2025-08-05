4ef55641418574628d56d2cbf5bcbd0d72b0b5f5
Darvidva <mbredavid8@gmail.com>
2025-08-05 06:07:43 +0100

    modified:   app/database.py

--- app/database.py
+++ app/database.py
@@ -1,37 +1,8 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
-import os
 from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
 
-# Load environment variables from .env file
 load_dotenv()
-
-# Get the DATABASE_URL from .env
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-# Optional: Raise error if DATABASE_URL is missing
-if not DATABASE_URL:
-    raise ValueError("DATABASE_URL is not set in the .env file")
-
-# Create SQLAlchemy engine
-engine = create_engine(DATABASE_URL)
-
-# Create a session factory
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Base class for your models
-Base = declarative_base()
-
-# Dependency for routes
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
 client = MongoClient(MONGO_URI)
-db = client["academate"]  # Use your DB name
+db = client["edudash_db"]  # Use your DB name
